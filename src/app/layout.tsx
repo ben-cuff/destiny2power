a01fce0f0e6e3b3227745fc28dba0a14744a0cd1
ben-cuff <bencuff14@gmail.com>
2024-04-07 19:38:03 -0400

    added icons and updated navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import NavBar from "./components/navigation/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <NavBar />
       <body className={inter.className}>{children}</body>
     </html>
   );
