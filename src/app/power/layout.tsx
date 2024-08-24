af10010d6a9083454f9ce138b43383d0ad9385a5
Ben Cuff <bencuff14@gmail.com>
2024-08-24 17:49:15 -0400

    added types to children in new layouts

--- src/app/power/layout.tsx
+++ src/app/power/layout.tsx
@@ -1,7 +1,11 @@
 import { getServerSession } from "next-auth/next";
 import { redirect } from "next/navigation";
 
-export default async function PowerLayout({ children }) {
+export default async function PowerLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
 	const session = await getServerSession();
 
 	if (!session) {
--- src/app/prep/layout.tsx
+++ src/app/prep/layout.tsx
@@ -1,7 +1,11 @@
 import { getServerSession } from "next-auth/next";
 import { redirect } from "next/navigation";
 
-export default async function PrepLayout({ children }) {
+export default async function PrepLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
 	const session = await getServerSession();
 
 	if (!session) {
