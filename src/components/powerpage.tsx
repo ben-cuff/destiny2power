cef051a7d8c97818ca796587f45987f03953f693
Ben Cuff <bencuff14@gmail.com>
2024-08-26 21:36:36 -0400

    added fractions to the display of powerlevel and powerlevel with bonus

--- src/components/powerpage.tsx
+++ src/components/powerpage.tsx
@@ -11,11 +11,24 @@ export default function PowerPage({
 	lightLevelBonus,
 	highestLightItems = [],
 }: PowerPageProps) {
+	const wholeNumber = Math.floor(lightLevel);
+	const decimalPart = lightLevel - wholeNumber;
+	const fraction = Math.round(decimalPart * 8);
+
+	const lightLevelString =
+		fraction === 0 ? `${wholeNumber}` : `${wholeNumber} ${fraction}/8`;
+
+	const lightLevelWithBonus = wholeNumber + lightLevelBonus;
+	const lightLevelStringWithBonus =
+		fraction === 0
+			? `${lightLevelWithBonus}`
+			: `${lightLevelWithBonus} ${fraction}/8`;
+
 	return (
 		<div>
 			<h1>Power Page</h1>
-			<p>Light Level: {lightLevel + lightLevelBonus}</p>
-			<p>Light Level without Bonus: {lightLevel}</p>
+			<p>Light Level: {lightLevelStringWithBonus}</p>
+			<p>Light Level without Bonus: {lightLevelString}</p>
 			<ul>
 				{highestLightItems.map(
 					(
