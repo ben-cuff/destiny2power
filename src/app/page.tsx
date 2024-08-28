6bb4a0520d0442e14070137d84218a385e55dff5
Ben Cuff <bencuff14@gmail.com>
2024-08-27 22:53:34 -0400

    removed unneccessary console logs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,3 @@
-"use client";
-import { useSession } from "next-auth/react";
-
 export default function HomePage() {
-	const { data: session } = useSession();
-
-	if (session) {
-		console.log("Access Token:", session.accessToken);
-	}
-
 	return <div>Home Page</div>;
 }
