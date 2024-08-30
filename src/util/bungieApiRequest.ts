6a0149107c574b9403b1c62d2a8be9cf21db7b47
ClearAspect <roanmason@live.ca>
2024-08-30 00:42:15 -0400

    Api request files

---
+++ src/util/bungieApiRequest.ts
@@ -0,0 +1,65 @@
+class ApiSession {
+	public accessToken: string;
+	public membershipType: number;
+	public membershipId: string;
+
+	constructor(
+		accessToken: string,
+		membershipType: number,
+		membershipId: string,
+	) {
+		this.accessToken = accessToken;
+		this.membershipType = membershipType;
+		this.membershipId = membershipId;
+	}
+
+	getAccessToken(): string {
+		return this.accessToken;
+	}
+
+	getMembershipType(): number {
+		return this.membershipType;
+	}
+
+	getMembershipId(): string {
+		return this.membershipId;
+	}
+}
+
+// 102: vault
+// 201: character inventory
+// 205: equipped items
+export enum BungieComponents {
+	PROFILE_INVENTORY = "102", //Vault
+	CHARACTER_INVENTORIES = "201", //Inventory of each character
+	CHARACTER_EQUIPMENT = "205", //Equipped items on each character
+}
+
+let apiSession: ApiSession;
+
+export function initializeApiSession(
+	accessToken: string,
+	membershipType: number,
+	membershipId: string,
+) {
+	apiSession = new ApiSession(accessToken, membershipType, membershipId);
+}
+
+export async function requestProfileComponent(component: BungieComponents) {
+	fetch(
+		`https://www.bungie.net/Platform/Destiny2/${apiSession.membershipType}/Profile/${apiSession.membershipId}/?components=${component}`,
+		{
+			headers: new Headers({
+				Authorization: `Bearer ${apiSession.accessToken}` || "",
+				"X-API-Key": process.env.BUNGIE_API_KEY || "",
+			}),
+		},
+	).then((response) => {
+		if (!response.ok) {
+			throw new Error(
+				`Error fetching component ${component}: ${response.status} ${response.statusText}`,
+			);
+		}
+		return response.json();
+	});
+}
