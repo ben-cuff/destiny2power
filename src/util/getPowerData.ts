119d25d93307d03923f40bba943531bd18377ada
Ben Cuff <bencuff14@gmail.com>
2024-08-29 22:49:24 -0400

    Revert "Merge branch 'dev' of https://github.com/ben-cuff/destiny2power into dev"
    This reverts commit eda75b3e428af15de9547c893f435bfa6ab6987a, reversing
    changes made to e7f85fdc6977dd24c24d7d601f4fc0167343c5b3.

--- src/util/getPowerData.ts
+++ src/util/getPowerData.ts
@@ -35,15 +35,13 @@ export async function fetchPowerData(
 	// this gets all of the details necessary to find the highest light items
 	// this includes data already gathered about plus its light level and item bucket
 	const itemDetailsPromises = combinedData.map(async (item) => {
-        const itemHash = await getItemBucket(item.itemHash);
-        const lightLevel = await getLightLevel(
-            membershipType,
-            membershipId,
-            item.itemInstanceId
-        );
+		const [itemHash, lightLevel] = await Promise.all([
+			getItemBucket(item.itemHash),
+			getLightLevel(membershipType, membershipId, item.itemInstanceId),
+		]);
 
-        return { item, itemHash, lightLevel };
-    });
+		return { item, itemHash, lightLevel };
+	});
 
 	const itemDetails = await Promise.all(itemDetailsPromises);
 
