690f0d68f4e202a828d07044324650cea827da7e
ClearAspect <roanmason@live.ca>
2024-08-29 23:03:34 -0400

    Refactor of getAllItems

--- src/util/getAllItems.ts
+++ src/util/getAllItems.ts
@@ -4,17 +4,17 @@
 export async function getAllItems(
 	membershipType: number,
 	membershipId: string,
-	accessToken: string,
+	accessToken: string
 ): Promise<any[]> {
+	// these are the three different components necessary to get all items
+	// 102: vault
+	// 201: character inventory
+	// 205: equipped items
+	const components = [102, 201, 205];
+	var fetchPromises: Promise<any>[];
 	try {
-		// these are the three different components necessary to get all items
-		// 102: vault
-		// 201: character inventory
-		// 205: equipped items
-		const components = [102, 201, 205];
-
 		// fetch data from the api
-		const fetchPromises = components.map((component) =>
+		fetchPromises = components.map((component) =>
 			fetch(
 				`https://www.bungie.net/Platform/Destiny2/${membershipType}/Profile/${membershipId}/?components=${component}`,
 				{
@@ -22,23 +22,24 @@ export async function getAllItems(
 						Authorization: `Bearer ${accessToken}` || "",
 						"X-API-Key": process.env.BUNGIE_API_KEY || "",
 					}),
-				},
+				}
 			).then((response) => {
 				if (!response.ok) {
 					throw new Error(
-						`Error fetching component ${component}: ${response.status} ${response.statusText}`,
+						`Error fetching component ${component}: ${response.status} ${response.statusText}`
 					);
 				}
 				return response.json();
-			}),
+			})
 		);
 
 		// combine the data to suit our needs
-		return createCombinedData(fetchPromises);
 	} catch (error) {
 		console.error("Failed to fetch items:", error);
 		throw error;
 	}
+
+	return createCombinedData(fetchPromises);
 }
 
 // Goes through the data, cleaning and combining into single object
@@ -54,11 +55,11 @@ async function createCombinedData(fetchPromises: Promise<any>[]) {
 		profileInventoryData.Response.profileInventory.data.items || [];
 
 	const inventoryItems = Object.values(
-		characterInventoriesData.Response.characterInventories.data,
+		characterInventoriesData.Response.characterInventories.data
 	).flatMap((character: any) => character.items || []);
 
 	const equippedItems = Object.values(
-		characterEquipmentData.Response.characterEquipment.data,
+		characterEquipmentData.Response.characterEquipment.data
 	).flatMap((character: any) => character.items || []);
 
 	// combines all of the data and removes everything without an instance id (we don't need these items)
