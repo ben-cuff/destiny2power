38f505e9effaaeb9a18e0d6d6a86a69763b3359d
Ben Cuff <bencuff14@gmail.com>
2024-09-02 22:24:08 -0400

    fixed copy error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function HomePage() {
 	return (
 		<div className="flex flex-col items-center justify-center align-middle">
-			<div className="text-3xl text-gray-300">Prep Page</div>
+			<div className="text-3xl text-gray-300">Home Page</div>
 			<div className="text-xl text-gray-300">
 				This page is a work in progress, check back later to see how it
 				looks
