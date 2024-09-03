ef6ea944f8bf61f40d060f83b46a2d14e8d7de3f
Ben Cuff <bencuff14@gmail.com>
2024-09-02 22:07:49 -0400

    added work in progress to work in progress pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
+import Image from "next/image";
+
 export default function HomePage() {
 	return (
-		<div>
-			<div>Home Page</div>
+		<div className="flex flex-col items-center justify-center align-middle">
+			<div className="text-3xl text-gray-300">Prep Page</div>
+			<div className="text-xl text-gray-300">
+				This page is a work in progress, check back later to see how it
+				looks
+			</div>
+
+			<Image
+				src="/work-in-progress.png"
+				width={500}
+				height={500}
+				alt="work in progress"
+			></Image>
 		</div>
 	);
 }
--- src/app/prep/page.tsx
+++ src/app/prep/page.tsx
@@ -1,3 +1,20 @@
+import Image from "next/image";
+
 export default function PrepPage() {
-	return <div>Prep Page</div>;
+	return (
+		<div className="flex flex-col items-center justify-center align-middle">
+			<div className="text-3xl text-gray-300">Prep Page</div>
+			<div className="text-xl text-gray-300">
+				This page is a work in progress, check back later to see how it
+				looks
+			</div>
+
+			<Image
+				src="/work-in-progress.png"
+				width={500}
+				height={500}
+				alt="work in progress"
+			></Image>
+		</div>
+	);
 }
