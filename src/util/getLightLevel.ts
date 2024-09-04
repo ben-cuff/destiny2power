8a1f5e1ff2d0ffc49052d3bea360a883141ad654
ClearAspect <roanmason@live.ca>
2024-09-04 00:10:46 -0400

    Refactored getLightLevel.ts

--- src/util/getLightLevel.ts
+++ src/util/getLightLevel.ts
@@ -1,34 +1,16 @@
 // this function gets the light level of a particular item
 
-export async function getLightLevel(
-	membershipType: number,
-	membershipId: string,
-	itemInstanceId: string,
-): Promise<number> {
-	try {
-		const response = await fetch(
-			`https://www.bungie.net/Platform/Destiny2/${membershipType}/Profile/${membershipId}/Item/${itemInstanceId}/?components=300`,
-			{
-				headers: new Headers({
-					"X-API-Key": process.env.BUNGIE_API_KEY || "",
-				}),
-			},
-		);
+import {
+	BungieComponents,
+	requestItemInstanceComponent,
+} from "./bungieApiRequest";
 
-		if (!response.ok) {
-			throw new Error(
-				`Error fetching light level: ${response.status} ${response.statusText}`,
-			);
-		}
+export async function getLightLevel(itemInstanceId: string): Promise<number> {
+	const data = await requestItemInstanceComponent(
+		BungieComponents.ITEM_INSTANCES,
+		itemInstanceId,
+	);
 
-		const data = await response.json();
-
-		// assigns the light level if it has one
-		const lightLevel = data.Response.instance.data?.primaryStat?.value || 0;
-
-		return lightLevel;
-	} catch (error) {
-		console.error("Failed to fetch light level:", error);
-		throw error;
-	}
+	const lightLevel = data.Response.instance.data?.primaryStat?.value || 0;
+	return lightLevel;
 }
