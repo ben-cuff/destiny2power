1fef7d1bb41e85522149cb014ec2e6eaf4bf61f4
ben-cuff <bencuff14@gmail.com>
2024-09-06 13:24:54 -0400

    added more metadata

--- src/app/power/layout.tsx
+++ src/app/power/layout.tsx
@@ -1,3 +1,29 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Destiny 2 Power",
+	description:
+		"A simple tool to optimize your power level grind. It allows you to both prepare for upcoming episodes and gain levels in the current episode. This page shows your highest light items and gives suggestions about the kinds of activities you should do",
+	keywords: [
+		"Destiny 2",
+		"power level",
+		"grind",
+		"Prep",
+		"Destiny",
+		"seasonal preparation",
+		"preparation",
+		"episode",
+		"power level",
+		"highest light",
+		"light level",
+		"equipment",
+		"items"
+	],
+	icons: {
+		icon: "tricorn.png",
+	},
+};
+
 export default async function PowerLayout({
 	children,
 }: {
--- src/app/prep/layout.tsx
+++ src/app/prep/layout.tsx
@@ -1,3 +1,24 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Destiny 2 Power",
+	description:
+		"A simple tool to optimize your power level grind. It allows you to both prepare for upcoming episodes and gain levels in the current episode. This page shows all the bounties you have and how much xp you have saved",
+	keywords: [
+		"Destiny 2",
+		"power level",
+		"grind",
+		"Prep",
+		"Destiny",
+		"seasonal preparation",
+		"preparation",
+		"episode",
+	],
+	icons: {
+		icon: "tricorn.png",
+	},
+};
+
 export default async function PrepLayout({
 	children,
 }: {
