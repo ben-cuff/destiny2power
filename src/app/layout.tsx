b95a5a4575643de514fed2642e7eaf53650bace6
ben-cuff <bencuff14@gmail.com>
2024-09-06 13:18:15 -0400

    updated metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,12 @@ import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
 	title: "Destiny 2 Power",
-	description: "A simple tool to optimize your power level grind.",
+	description:
+		"A simple tool to optimize your power level grind. It allows you to both prepare for upcoming episodes and gain levels in the current episode",
+	keywords: ["Destiny 2", "power level", "grind", "Prep", "Destiny"],
+	icons: {
+		icon: "tricorn.png",
+	},
 };
 
 export default function RootLayout({
